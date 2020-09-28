6ab30221a51d4e6e4eb4791ca7c2de510fe70cd5
kostik4kg <kostik4kg@gmail.com>
2020-09-28 22:12:25 +0300

    feat: add function min max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((array === 'undefined') || (!Array.isArray(array)) || (array.length === 0)){
+  return 0;}
+  else{
+    let arrMin = array.sort(function(a,b){
+      return a>b? 1:-1;
+    });
+    return arrMin[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((array === 'undefined') || (!Array.isArray(array)) || (array.length === 0)) {
+    return 0;
+  }
+  else {
+    let arrMax =  array.sort(function (a, b) {
+      return a < b ? 1 : -1;
+    });
+    return arrMax[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((array === 'undefined') || (!Array.isArray(array)) || (array.length === 0)) {
+    return 0;
+  }
+  else {
+    let sum = 0;
+    array.forEach(element => {
+      sum += element;
+    });
+    return sum / array.length;
+  }
 }
